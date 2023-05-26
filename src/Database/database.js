3ff4957afe839fbe4bbf168b6416dd119ae0de4e
Saurabh Singh <67182965+triver-124@users.noreply.github.com>
2023-05-26 15:43:19 +0530

    Delete src/Database directory

--- src/Database/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/Isa-aug")
-.then(() => { 
-    console.log("connection is sucessful");   
-}).catch((err) => {
-    console.log("no connection");
-})
